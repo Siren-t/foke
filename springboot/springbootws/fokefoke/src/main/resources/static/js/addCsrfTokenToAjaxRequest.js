0b449ccabb2b449d3d28b290cac0613b13702953
122friendly <122friendly@gmail.com>
2023-07-06 17:11:23 +0900

    dev

--- springboot/springbootws/fokefoke/src/main/resources/static/js/addCsrfTokenToAjaxRequest.js
+++
@@ -1,5 +0,0 @@
-function addCsrfTokenToAjaxRequest(xhr) {
-    var token = $("meta[name='_csrf']").attr("content");
-    var header = $("meta[name='_csrf_header']").attr("content");
-    xhr.setRequestHeader(header, token);
-}
